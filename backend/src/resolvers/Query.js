7f13dd23ba1505a690e7712a7513c2ee83efaf87
niljimenezjr <niljimenezjr@gmail.com>
2020-07-27 23:51:05 +0800

    item creation prisma yoga

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,9 @@
-const Query = {};
+const Query = {
+    async items(parent, args, ctx, info) {
+        console.log("Getting Items!");
+        const items = await ctx.db.query.items();
+        return items
+    }
+};
 
 module.exports = Query;
